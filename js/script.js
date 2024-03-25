02decf00f661d5350ffd60f74083c6864c57aa68
Jop <jopmolenaar@icloud.com>
2024-03-25 10:29:16 +0100

    data fetched

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("hoi");
+let allData = [];
+
+const yearsData = fetch("https://cssday.nl/data.json").then((response) => response.json());
+const speakersData = fetch("https://cssday.nl/data/speakers.json").then((response) => response.json());
+const talksData = fetch("https://cssday.nl/data/talks.json").then((response) => response.json());
+
+Promise.all([yearsData, speakersData, talksData])
+    .then(([data1, data2, data3]) => {
+        allData.push(data1, data2, data3);
+        logData(allData);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+function logData(data) {
+    console.log(data);
+}
