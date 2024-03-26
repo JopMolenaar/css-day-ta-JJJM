6d41e1f810decca03eea2fa52ebc084a7819bb11
Juriën Waijenberg <jurien.waijenberg@hva.nl>
2024-03-26 11:12:45 +0100

    removing dummydata when real data is loaded

--- docs/js/dataCalc.js
+++ docs/js/dataCalc.js
@@ -3,130 +3,133 @@ let alreadyGotCountryCodes = []
 const yearButtons = document.querySelectorAll('.yearButton')
 
 async function dataCalc() {
-	const data = await dataPromise
-	if (data) {
-		for (const [year, info] of Object.entries(data)) {
-			cloneInfoSections(year, info, data)
-		}
-	} else {
-		console.error('Failed to fetch data.')
-	}
+    const data = await dataPromise
+    if (data) {
+        for (const [year, info] of Object.entries(data)) {
+            cloneInfoSections(year, info, data)
+        }
+    } else {
+        console.error('Failed to fetch data.')
+    }
 }
 
 function giveCountryAColor(year, countryWithCount, themeColor, svg) {
-	const paths = svg.querySelectorAll(`path`)
-	const descOfSvg = document.getElementById(`desc-${year}`)
-	paths.forEach((path) => {
-		path.setAttribute('style', `rgb(124, 124, 124)`)
-	})
-	let highestNumber = 1
-	for (const [country, count] of Object.entries(countryWithCount)) {
-		if (highestNumber < count) {
-			highestNumber = count
-		}
-	}
-	const visitedCountries = []
-	for (const [country, count] of Object.entries(countryWithCount)) {
-		// console.log(`country: ${country}, count: ${count}`);
-		paths.forEach((path) => {
-			if (path.dataset.country) {
-				if (country === path.dataset.country) {
-					const color1 = themeColor
-					const color2 = '#ffffff' // White
+    const paths = svg.querySelectorAll(`path`)
+    const descOfSvg = document.getElementById(`desc-${year}`)
+    paths.forEach((path) => {
+        path.setAttribute('style', `rgb(124, 124, 124)`)
+    })
+    let highestNumber = 1
+    for (const [country, count] of Object.entries(countryWithCount)) {
+        if (highestNumber < count) {
+            highestNumber = count
+        }
+    }
+    const visitedCountries = []
+    for (const [country, count] of Object.entries(countryWithCount)) {
+        // console.log(`country: ${country}, count: ${count}`);
+        paths.forEach((path) => {
+            if (path.dataset.country) {
+                if (country === path.dataset.country) {
+                    const color1 = themeColor
+                    const color2 = '#ffffff' // White
 
-					// Define the mix ratio (e.g., 30% white)
-					let mixRatio = 0
-					if (path.dataset.country !== 'NL') {
-						mixRatio = 0.7 - (count / highestNumber) * 2
-					}
+                    // Define the mix ratio (e.g., 30% white)
+                    let mixRatio = 0
+                    if (path.dataset.country !== 'NL') {
+                        mixRatio = 0.7 - (count / highestNumber) * 2
+                    }
 
-					// Convert the colors to RGB values
-					const rgbColor1 = hexToRgb(color1)
-					const rgbColor2 = hexToRgb(color2)
+                    // Convert the colors to RGB values
+                    const rgbColor1 = hexToRgb(color1)
+                    const rgbColor2 = hexToRgb(color2)
 
-					// Calculate the mixed color manually
-					const mixedColor = mixColors(rgbColor1, rgbColor2, mixRatio)
+                    // Calculate the mixed color manually
+                    const mixedColor = mixColors(rgbColor1, rgbColor2, mixRatio)
 
-					// Set the mixed color as the fill attribute of the path element
-					path.setAttribute(
-						'style',
-						`fill: rgb(${mixedColor.r}, ${mixedColor.g}, ${mixedColor.b})`
-					)
+                    // Set the mixed color as the fill attribute of the path element
+                    path.setAttribute(
+                        'style',
+                        `fill: rgb(${mixedColor.r}, ${mixedColor.g}, ${mixedColor.b})`
+                    )
 
-					if (!alreadyGotCountryCodes.includes(country)) {
-						alreadyGotCountryCodes.push(country)
-					}
-				}
+                    if (!alreadyGotCountryCodes.includes(country)) {
+                        alreadyGotCountryCodes.push(country)
+                    }
+                }
 
-				if (!countryCodes.includes(country)) {
-					countryCodes.push(country)
-				}
-			}
-		})
-		visitedCountries.push(`${country} with ${count} visitors`)
-	}
-	countryCodes.forEach((code) => {
-		if (!alreadyGotCountryCodes.includes(code) && code !== 'SG') {
-			console.error('missing:', code)
-		}
-	})
-	const visitedCountriesString = visitedCountries.join(', ')
-	descOfSvg.textContent = `The world map shows with the theme color where all the visitors come from. 
+                if (!countryCodes.includes(country)) {
+                    countryCodes.push(country)
+                }
+            }
+        })
+        visitedCountries.push(`${country} with ${count} visitors`)
+    }
+    countryCodes.forEach((code) => {
+        if (!alreadyGotCountryCodes.includes(code) && code !== 'SG') {
+            console.error('missing:', code)
+        }
+    })
+    const visitedCountriesString = visitedCountries.join(', ')
+    descOfSvg.textContent = `The world map shows with the theme color where all the visitors come from. 
     The team color is currently: purple. 
     The colored countries are: ${visitedCountriesString}.`
 }
 
 function hexToRgb(hex) {
-	// Remove the '#' character if present
-	hex = hex.replace('#', '')
+    // Remove the '#' character if present
+    hex = hex.replace('#', '')
 
-	// Parse the hex color into RGB components
-	const r = parseInt(hex.substring(0, 2), 16)
-	const g = parseInt(hex.substring(2, 4), 16)
-	const b = parseInt(hex.substring(4, 6), 16)
+    // Parse the hex color into RGB components
+    const r = parseInt(hex.substring(0, 2), 16)
+    const g = parseInt(hex.substring(2, 4), 16)
+    const b = parseInt(hex.substring(4, 6), 16)
 
-	// Return an object with the RGB values
-	return { r, g, b }
+    // Return an object with the RGB values
+    return { r, g, b }
 }
 
 // Function to mix two RGB colors with a specified ratio
 function mixColors(color1, color2, ratio) {
-	// Calculate the mixed RGB components
-	const mixedR = Math.round((1 - ratio) * color1.r + ratio * color2.r)
-	const mixedG = Math.round((1 - ratio) * color1.g + ratio * color2.g)
-	const mixedB = Math.round((1 - ratio) * color1.b + ratio * color2.b)
+    // Calculate the mixed RGB components
+    const mixedR = Math.round((1 - ratio) * color1.r + ratio * color2.r)
+    const mixedG = Math.round((1 - ratio) * color1.g + ratio * color2.g)
+    const mixedB = Math.round((1 - ratio) * color1.b + ratio * color2.b)
 
-	// Return the mixed color as an RGB object
-	return { r: mixedR, g: mixedG, b: mixedB }
+    // Return the mixed color as an RGB object
+    return { r: mixedR, g: mixedG, b: mixedB }
 }
 
 function cloneInfoSections(year, info, data) {
-	const template = document.getElementById('template')
-	const infoSection = document.querySelector('.info')
+    const template = document.getElementById('template')
+    const infoSection = document.querySelector('.info')
 
-	const firstClone = template.content.cloneNode(true)
-	const sections = firstClone.querySelectorAll('section > section')
-	sections.forEach((section) => {
-		section.style.background = info.color.hex
-	})
+    const firstClone = template.content.cloneNode(true)
+    const sections = firstClone.querySelectorAll('section > section')
+    sections.forEach((section) => {
+        section.style.background = info.color.hex
+    })
 
-	// TODO hier zoeken naar de elementen om the veranderen
-	// dan data[year]. iets wat je nodig hebt uit de data als textContent of url of src
+    // TODO hier zoeken naar de elementen om the veranderen
+    // dan data[year]. iets wat je nodig hebt uit de data als textContent of url of src
 
-	// Some changes of the id in the svg in the template
-	const map = firstClone.querySelector('section > section svg')
-	const title = firstClone.querySelector('section > section svg title')
-	const desc = firstClone.querySelector('section > section svg desc')
-	title.id = `title-${year}`
-	desc.id = `desc-${year}`
-	map.setAttribute('aria-labelledby', `title-${year}`)
-	map.setAttribute('aria-describedby', `desc-${year}`)
+    // Some changes of the id in the svg in the template
+    const map = firstClone.querySelector('section > section svg')
+    const title = firstClone.querySelector('section > section svg title')
+    const desc = firstClone.querySelector('section > section svg desc')
+    title.id = `title-${year}`
+    desc.id = `desc-${year}`
+    map.setAttribute('aria-labelledby', `title-${year}`)
+    map.setAttribute('aria-describedby', `desc-${year}`)
 
-	const countryWithCount = data[year].attendees.countries // data for the year
-	const themeColor = data[year].color.hex // theme color for the year
+    const countryWithCount = data[year].attendees.countries // data for the year
+    const themeColor = data[year].color.hex // theme color for the year
 
-	infoSection.appendChild(firstClone) // append template to section
-	giveCountryAColor(year, countryWithCount, themeColor, map) // fill in the map colors
+    infoSection.appendChild(firstClone) // append template to section
+
+    const dummyData = document.getElementById('dummy-data')
+    dummyData.remove()
+    giveCountryAColor(year, countryWithCount, themeColor, map) // fill in the map colors
 }
 
 dataCalc()
