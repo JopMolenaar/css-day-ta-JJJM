0ab538c291a4a39ce67bc7d7f5b3746cd232060d
Jop <jopmolenaar@icloud.com>
2024-03-27 10:32:45 +0100

    svg is now a svg to animate

--- docs/js/dataCalc.js
+++ docs/js/dataCalc.js
@@ -156,21 +156,12 @@ async function cloneInfoSections(year, info, data) {
 		extra = restOfText.substring(plusIndex + 1); // Extract text after the "+"
 		restOfText = restOfText.substring(0, plusIndex);
 	}
-	const imgElement = document.createElement('img');
-	imgElement.src = 'https://cssday.nl/_img/cssday-logo.svg';
-	imgElement.setAttribute('aria-label', '');
-	titleEvent.innerHTML = `<span class="visually-hidden">${cssDay}</span><span class="rotate">${restOfText}</span>`;
-
-	const span = document.createElement('span');
-	span.textContent = extra;
-	span.classList.add('extra');
-	titleEvent.appendChild(span);
-	titleEvent.appendChild(imgElement);
-
-
-
-
-
+	const hiddenText = titleEvent.querySelector('.visually-hidden');
+	hiddenText.textContent = cssDay;
+	const rotateYear = titleEvent.querySelector('.rotate');
+	rotateYear.textContent = restOfText;
+	const specialText = titleEvent.querySelector('.extra');
+	specialText.textContent = extra;
 
 	const mc = firstClone.querySelector('.mc');
 
