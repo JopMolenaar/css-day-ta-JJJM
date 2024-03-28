9628f810f54ba8f6d0c846a769aa7c8c612e82a2
Manoah Tervoort <149895ja@gmail.com>
2024-03-28 15:06:20 +0100

    fix: fail one in ten

--- docs/js/api.js
+++ docs/js/api.js
@@ -18,7 +18,7 @@ async function fetchData() {
 		const data = await result.json();
 
 		const willFail =
-			window.location.protocol === 'https:' && Math.random() < 0.5;
+			window.location.protocol === 'https:' && Math.random() < 0.1;
 		if (willFail) throw new Error('Oops! Something went wrong...');
 
 		console.debug('Data fetched', data);
