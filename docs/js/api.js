4e76a710ead105e757875eed6b26b6258cca3e66
Jop <jopmolenaar@icloud.com>
2024-06-05 10:53:41 +0200

    data is now fetched from local files

--- docs/js/api.js
+++ docs/js/api.js
@@ -14,7 +14,8 @@
 async function fetchData() {
 	try {
 		console.debug('Fetching data...');
-		const result = await fetch('https://cssday.nl/data.json');
+		// const result = await fetch('https://cssday.nl/data.json');
+		const result = await fetch('../data/data.json');
 		const data = await result.json();
 
 		const willFail =
